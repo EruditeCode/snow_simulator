ddb79d95e40c0e5383beabf68b845e5eb06e4c5f
EruditeCode <erudite.home@gmail.com>
2022-12-05 10:51:38 +0000

    Used pygame draw function to draw the snowflakes in main.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
 		
 		# Draw the snowflakes.
 		for snowflake in snowflakes:
-			snowflake.draw(screen, HEIGHT)
+			pygame.draw.circle(screen, snowflake.color, snowflake.pos, snowflake.radius)
 			snowflake.controller(HEIGHT, WIDTH)
 
 		screen.blit(trees_front, (0, 0))	
