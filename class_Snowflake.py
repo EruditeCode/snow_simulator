58dfc2a0b26135e268de4d17fa41855c83237f42
EruditeCode <erudite.home@gmail.com>
2022-12-05 10:42:25 +0000

    Added adaptable radii for each snowflake.

--- class_Snowflake.py
+++ class_Snowflake.py
@@ -8,15 +8,25 @@ class Snowflake:
 		self.pos = position
 		self.radius = radius
 		self.gravity = gravity
+		self.RADIUS_MAX = radius
+		self.radius_change = True
 
 	def controller(self, height, width):
 		self.update_pos(height, width)
+		self.update_rad()
 
 	def update_pos(self, height, width):
 		if self.pos[1] < height + self.radius:
 			self.pos = (self.pos[0] + randint(-1, 1), self.pos[1] + self.gravity)
 		else:
 			self.respawn(width)
+
+	def update_rad(self):
+		if self.radius_change:
+			self.radius = randint(1, self.RADIUS_MAX)
+			self.radius_change = False
+		else:
+			self.radius_change = True
 
 	def respawn(self, width):
 	 	self.pos = (randint(0, width), -1 * self.radius)
