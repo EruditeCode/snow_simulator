1c9dd84eabfef655de2024445b4540536c36c186
EruditeCode <erudite.home@gmail.com>
2022-12-05 09:45:40 +0000

    Updated with respawn function and wobble in x-plane.

--- class_Snowflake.py
+++ class_Snowflake.py
@@ -9,6 +9,18 @@ class Snowflake:
 		self.radius = radius
 		self.gravity = gravity
 
+	def controller(self, height, width):
+		self.update_pos(height, width)
+
+	def update_pos(self, height, width):
+		if self.pos[1] < height + self.radius:
+			self.pos = (self.pos[0] + randint(-1, 1), self.pos[1] + self.gravity)
+		else:
+			self.respawn(width)
+
+	def respawn(self, width):
+	 	self.pos = (randint(0, width), -1 * self.radius)
+
 	def draw(self, screen):
 		pygame.draw.circle(screen, (250, 250, 250), self.pos, self.radius)
 
